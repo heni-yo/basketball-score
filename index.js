ccba9166f4f512c3fea6d0f83731e949b214842c
Mohamed Heni Baabaa <84690768+heni-yo@users.noreply.github.com>
2023-09-05 22:30:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,70 +1,44 @@
-let homeScore = document.getElementById("home-score")
-let guestScore = document.getElementById("guest-score")
+let homeScore = document.getElementById("home-score");
+let guestScore = document.getElementById("guest-score");
 
-let homeWinner = document.getElementById("home-winner")
-let guestWinner = document.getElementById("guest-winner")
+let homeWinner = document.getElementById("home-winner");
+let guestWinner = document.getElementById("guest-winner");
 
-let homeCount = 0
-let guestCount = 0
-////////
-function plusOneH(){
-    homeCount += 1
-    homeScore.innerText = homeCount
-    whoIsTheWinner()
+let homeCount = 0;
+let guestCount = 0;
+
+function scoreUpdate(team, point) {
+  if (team === "home") {
+    homeCount += point;
+    homeScore.innerText = homeCount;
+    whoIsTheWinner();
+  }
+
+  if (team === "guest") {
+    guestCount += point;
+    guestScore.innerText = guestCount;
+    whoIsTheWinner();
+  }
 }
 
-function plusTwoH(){
-    homeCount += 2
-    homeScore.textContent = homeCount
-    whoIsTheWinner()
+function Reset() {
+  homeScore.textContent = 0;
+  guestScore.textContent = 0;
+  homeCount = 0;
+  guestCount = 0;
 }
 
-function plusThreeH(){
-    homeCount += 3
-    homeScore.textContent = homeCount
-    whoIsTheWinner()
+function whoIsTheWinner() {
+  if (homeCount > guestCount) {
+    homeWinner.textContent = "winner";
+    guestWinner.textContent = "loser";
+  }
+  if (homeCount < guestCount) {
+    homeWinner.textContent = "loser";
+    guestWinner.textContent = "winner";
+  }
+  if (homeCount === guestCount) {
+    homeWinner.textContent = "draw";
+    guestWinner.textContent = "drew";
+  }
 }
-/////////
-function plusOneG(){
-    guestCount += 1
-    guestScore.textContent = guestCount
-    whoIsTheWinner()
-}
-
-function plusTwoG(){
-    guestCount += 2
-    guestScore.textContent = guestCount
-    whoIsTheWinner()
-}
-
-function plusThreeG(){
-    guestCount += 3
-    guestScore.textContent = guestCount
-    
-    whoIsTheWinner()
-}
-/////////
-function Reset(){
-    homeScore.textContent = 0
-    guestScore.textContent = 0
-    homeCount = 0
-    guestCount = 0
-}
-
-function whoIsTheWinner(){
-        
-    if(homeCount>guestCount){
-        homeWinner.textContent = "winner"
-        guestWinner.textContent = "loser"
-    }
-    if(homeCount<guestCount){
-        homeWinner.textContent =  "loser"
-        guestWinner.textContent = "winner"
-    }
-    if(homeCount===guestCount){
-        homeWinner.textContent = "draw"
-        guestWinner.textContent = "drew"
-    }
-}
-
-
